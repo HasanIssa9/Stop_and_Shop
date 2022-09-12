0c64a6c9d59fbafdd577047f8a341bab9de73f63
abd2oo2 <cs.20.156@student.uotechnology.edu.iq>
2022-09-12 10:41:58 +0300

    set location

---
+++ frontend/lib/Screens/Setlocation/SetLocation.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:geolocator/geolocator.dart';
+
+class SetLocation extends StatelessWidget {
+  const SetLocation({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child:Scaffold(
+        
+      )
+        );
+  }
+}
