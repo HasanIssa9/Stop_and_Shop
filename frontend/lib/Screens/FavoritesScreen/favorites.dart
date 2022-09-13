192fcfc98093e9f80ac6b0211517bb81eed6d536
Hasan Issa <96444548+HasanIssa9@users.noreply.github.com>
2022-09-13 21:44:33 +0300

    auth

--- frontend/lib/Screens/FavoritesScreen/favorites.dart
+++ frontend/lib/Screens/FavoritesScreen/favorites.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:stop_and_shop/shared/components/components.dart';
+import '../../Services/local_database/shared_preferences.dart';
 import '../../modules/module_product.dart';
+import '../../style/colors.dart';
+import '../CheckScreens/login.dart';
+import '../CheckScreens/register.dart';
 import '../HomeScreen/build_containers.dart';
 import '../HomeScreen/search.dart';
 import '../Details/details.dart';
@@ -21,48 +25,95 @@ class Favorites extends StatelessWidget {
           titleSpacing: 20,
           title: TitleName(title: 'المفضلة'),
         ),
-        body: Container(
-          width: double.infinity,
-          padding: EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // ----------------------------- Box title ----------------------------------------
+        body: (Database.prefs.getString('email')!.isNotEmpty)
+            ? Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // ----------------------------- Box title ----------------------------------------
 
-              // ----------------------------- Box search ---------------------------------------
-              Search(),
-              const SizedBox(
-                height: 15,
-              ),
-              // ------------------------------- List the product ---------------------------------
-              Expanded(
-                child: Obx(() {
-                  return ListView(
-                    children: [
-                      Wrap(
-                        alignment: WrapAlignment.center,
-                        children: Product.products
-                            .where((e) => e.isFavorite.value == true)
-                            .map((e) => GestureDetector(
-                          onTap: (){
-                            Get.to(Details(product: e,));
-                          },
-                                    child: BuildItem(
-                                  imageUrl: e.imageProduct.value,
-                                  nameProduct: e.nameProduct.value,
-                                  categoryProduct: e.categoryProduct.value,
-                                  price: e.priceProduct.value,
-                                  product: e,
-                                )))
-                            .toList(),
-                      )
-                    ],
-                  );
-                }),
+                    // ----------------------------- Box search ---------------------------------------
+                    Search(),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    // ------------------------------- List the product ---------------------------------
+                    Expanded(
+                      child: Obx(() {
+                        return ListView(
+                          children: [
+                            Wrap(
+                              alignment: WrapAlignment.center,
+                              children: Product.products
+                                  .where((e) => e.isFavorite.value == true)
+                                  .map((e) => GestureDetector(
+                                      onTap: () {
+                                        Get.to(Details(
+                                          product: e,
+                                        ));
+                                      },
+                                      child: BuildItem(
+                                        imageUrl: e.imageProduct.value,
+                                        nameProduct: e.nameProduct.value,
+                                        categoryProduct:
+                                            e.categoryProduct.value,
+                                        price: e.priceProduct.value,
+                                        product: e,
+                                      )))
+                                  .toList(),
+                            )
+                          ],
+                        );
+                      }),
+                    )
+                  ],
+                ),
               )
-            ],
-          ),
-        ),
+            : Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ContainerDesgin(
+                        color: greenColor,
+                        child: TextButton(
+                          onPressed: () {
+                            Get.to(Login());
+                          },
+                          child: const Text(
+                            'تسجيل الدخول',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10,),
+                      ContainerDesgin(
+                        color: greenColor,
+                        child: TextButton(
+                          onPressed: () {
+                            Get.to(Signup());
+                          },
+                          child: const Text(
+                            'تسجيل',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
       ),
     );
   }
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:stop_and_shop/Screens/Setlocation/set_location.dart';
+import 'package:stop_and_shop/Screens/LoadingScreen/loading_screen.dart';
 import 'package:stop_and_shop/Screens/SplashScreen/splash_screen.dart';
+import 'package:stop_and_shop/Services/local_database/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-import 'Screens/FirstScreen/first_screen.dart';
 
 
-void main() {
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
   ));
+
+  Database.prefs=await SharedPreferences.getInstance();
   runApp(const MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -22,7 +28,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: Loading(),
 
     );
   }
